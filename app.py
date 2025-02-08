5a47b4be99f609c4c8e4d9162b6b14d98f932a40
Deepansh Garg <148648766+Deepcoder2905@users.noreply.github.com>
2025-02-08 18:22:08 +0530

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from langchain_huggingface import HuggingFaceEmbeddings
 import json
 import random
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder="static", template_folder="templates")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.sqlite3'
 app.config['SECRET_KEY'] = 'Deepubhai'
 app.config['SECURITY_PASSWORD_SALT'] = 'Deepu'
